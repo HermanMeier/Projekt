75645bb8321aed31775d38140bc6809d9c660357
Herman Meier <herman.meier@estcube.eu>
2016-12-15 02:31:25 +0200

    Esitamiseks valmis

--- projekt.py
+++ projekt.py
@@ -1,111 +1,239 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import messagebox
-from tkinter import filedialog
+from tkinter import filedialog, colorchooser, messagebox
 from pathlib import Path
 import time, os, sys, shutil
-from PIL import Image, ImageTk, ImageGrab
+from PIL import Image, ImageTk, ImageGrab, ImageDraw
 #from Image import ImageTk
 
 raam=Tk()
 raam.title("Projekt")
-tahvel = Canvas(raam, width=500, height=500, background="white")
+tahvli_aken = Toplevel()
+tahvli_aken.title("Pilt")
+tahvel = Canvas(tahvli_aken, width=500, height=500, background="white")
 
 #Joone omadused
 paksus=IntVar()
 paksus.set(2)
 värv=StringVar()
-värv.set("red")
-pilt=0
+värv.set("#000000")
+kujundi_piir=StringVar()
+värvi_indikaator=0
+
+#PIL image ja sellele joonistamine
+pilt=Image.new("P", (500,500), color=255)
+joonista = ImageDraw.Draw(pilt)
+
 #Teksti omadused
-sisend=StringVar()
-#Siia tuleb veel font
+sisendtekst=StringVar()
 
 #Hiire positsioon
-#ehiir on hiire positsioon "delay" sekundit tagasi, selle abil saab enamvähem sujuvalt joonistada
+#ehiir on eelmine hiire positsioon
 hiir_x=0
 hiir_y=0
 ehiir_x=0
 ehiir_y=0
-delay=0.02
+hiire_info=StringVar()
+hiire_info.set("")
+
+#Kujundite joonistamiseks
+algus_x=-1
+algus_y=-1
 
 def init_GUI():
-    #_b- button,  _l- dropdown, _c- checkbox, _e entry
+    #_b- button,  _d- dropdown, _c- checkbox, _e entry, _l- label
     
-    #Joonistamis ala
+    #Joonistamis ala ja algne hiire kontroll
     tahvel.grid(column=0, row=1, padx=5, pady=5, sticky=(E,N,S,W))
     tahvel.bind("<B1-Motion>", pliiats)
     tahvel.bind("<Motion>", hiire_positsioon)
+    #tahvel.bind_all("<MouseWheel>", zoom_window)
+    
     #Kõikide nuppude raam
     toolbar = ttk.Frame(height=2,relief=SUNKEN)
     toolbar.grid(column=0, row=0, padx=5, pady=5, sticky=(E,N,S,W))
-    #Pliiatsi valimis nupp
+    
+    #Pliiatsi valimise nupp
     def pliiatsi_kasutus():
         tahvel.bind("<B1-Motion>", pliiats)
         tahvel.bind("<Button-1>", ignore)
+        tahvel.bind("<ButtonRelease-1>", ignore)
     pliiats_b = ttk.Button(toolbar, text="Pliiats", command=pliiatsi_kasutus)
     pliiats_b.grid(column=0, row=0, padx=5, pady=5, sticky=(E,N,S,W))
-    #Pintsli valimis nupp
-    def pintsli_kasutus():
-        tahvel.bind("<B1-Motion>", pintsel)
+    
+    #Paksuse valik
+    global paksus
+    paksus_l = ttk.Label(toolbar, text="Paksus:")
+    paksus_l.grid(column=1, row=0, padx=5, pady=5, sticky=(N,S,W))
+    paksus_e = ttk.Entry(toolbar, textvariable=paksus)
+    paksus_e.grid(column=2, row=0, padx=5, pady=5, sticky=(N,S,W))
+    
+    #Kujundi valimine
+    def kujundi_kasutus():
+        tahvel.bind("<B1-Motion>", kujund)
         tahvel.bind("<Button-1>", ignore)
-    pintsel_b = ttk.Button(toolbar, text="Pintsel", command=pintsli_kasutus)
-    pintsel_b.grid(column=1, row=0, padx=5, pady=5, sticky=(E,N,S,W))
-    #Teksti valimis nupp
+        tahvel.bind("<ButtonRelease-1>", kujund_reset)
+    piirid = ["Piirjoon", "Kuju", "Piiriga kuju"]
+    kujund_b = ttk.Button(toolbar, text="Kujund", command=kujundi_kasutus)
+    kujund_b.grid(column=3, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    kujundi_piir_d = ttk.OptionMenu(toolbar, kujundi_piir , piirid, *piirid)
+    kujundi_piir_d.grid(column=4, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    kujundi_piir.set("Kuju")
+    
+    #Teksti valimise nupp
     def teksti_kasutus():
         tahvel.bind("<B1-Motion>", ignore)
         tahvel.bind("<Button-1>", tekst)
-    tekst_b = ttk.Button(toolbar, text="Tekst", command=teksti_kasutus)
-    tekst_b.grid(column=2, row=0, padx=5, pady=5, sticky=(E,N,S,W))
-    #Paksuse valik
-    global paksus
-    paksus_e = ttk.Entry(toolbar, textvariable=paksus)
-    paksus_e.grid(column=3, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+        tahvel.bind("<ButtonRelease-1>", ignore)
+    tekst_b = ttk.Button(toolbar, text="Kuva see tekst:", command=teksti_kasutus)
+    tekst_b.grid(column=5, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
     #Värvi valik
-    global värv
-    VÄRVID=[" ","yellow","black", "red", "blue", "green"]
-    värv_l = ttk.OptionMenu(toolbar, värv, *VÄRVID)
-    värv_l.grid(column=4, row=0, padx=5, pady=5, sticky=(E,N,S,W))
-    värv.set("red")
+    värv.set("#000000")
+    uus_värv_b = ttk.Button(toolbar, text="Uus värv", command=uus_värv)
+    uus_värv_b.grid(column=7, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
     #Tekst mida saab kuvada
-    tekst_e = ttk.Entry(toolbar, textvariable=sisend)
-    tekst_e.grid(column=5, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    tekst_e = ttk.Entry(toolbar, textvariable=sisendtekst)
+    tekst_e.grid(column=6, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
     #Vali pilt mida kuvada
     ava_pilt_b = ttk.Button(toolbar, text="Ava pilt", command=ava_pilt)
-    ava_pilt_b.grid(column=6, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    ava_pilt_b.grid(column=8, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
     #Pildi salvestamine
     salvesta_pilt_b = ttk.Button(toolbar, text="Salvesta pilt", command=salvesta_pilt)
-    salvesta_pilt_b.grid(column=7, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    salvesta_pilt_b.grid(column=9, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
+    #Pildi lõikamine
+    def pildi_lõikamise_kasutus():
+        tahvel.bind("<B1-Motion>", lõika_pilt)
+        tahvel.bind("<Button-1>", ignore)
+        tahvel.bind("<ButtonRelease-1>", lõika_pilt_reset)
+    lõika_pilt_b = ttk.Button(toolbar, text="Lõika", command=pildi_lõikamise_kasutus)
+    lõika_pilt_b.grid(column=10, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+    
+    #Pildi pööramine
+    pööra_pilt_b = ttk.Button(toolbar, text="Pööra", command=pildi_pööramine)
+    pööra_pilt_b.grid(column=11, row=0, padx=5, pady=5, sticky=(E,N,S,W))
+
+
+    #Alumine inforiba
+    infobar = ttk.Frame(height=2,relief=SUNKEN)
+    infobar.grid(column=0, row=2, padx=5, pady=5, sticky=(E,N,S,W))
+
+    #Hiire positsioon
+    hiire_info_l = ttk.Label(infobar, textvariable=hiire_info)
+    hiire_info_l.grid(column=0, row=0, padx=5, pady=5, sticky=(N,S,W))
+
+    #Värv
+    global värvi_info
+    värvi_info = Canvas(infobar, width=100, height=infobar.winfo_height(), background=värv.get())
+    värvi_info.grid(column=1, row=0, padx=5, pady=5, sticky=(N,S,W))
+    värvi_indikaator = värvi_info.create_rectangle(0,0,värvi_info.winfo_width(),värvi_info.winfo_height(), fill=värv.get())
+    
 #Kui on vaja et klahv nupp midagi ei teeks
 def ignore(event):
     return 0
+#Paletilt uue värvi valimine
+def uus_värv():
+    uus_värv=str(colorchooser.askcolor(initialcolor='#ff0000'))
+    uus_värv=uus_värv[uus_värv.find("#"):len(uus_värv)-2]
+    värv.set(uus_värv)
+    global värvi_info
+    värvi_info.delete("all")
+    värvi_info.create_rectangle(0,0,värvi_info.winfo_width(),värvi_info.winfo_height(), fill=värv.get())
+    kuva_pilt(pilt)
 #Hiire koordinaatide registreerimine
 def hiire_positsioon(event):
-    global hiir_x, hiir_y, ehiir_x, ehiir_y, delay
+    global hiir_x, hiir_y, ehiir_x, ehiir_y, hiire_info
     ehiir_x=hiir_x
     ehiir_y=hiir_y
     hiir_x=event.x
     hiir_y=event.y
-    time.sleep(delay)
+    hiire_info.set("Hiir: " + str(hiir_x) + "x" + str(hiir_y))
+#Rullikuga zoom
+def zoom_window(event):
+    if event.delta < 0: faktor=-0.01
+    else:   faktor=0.01
+    tahvel.config(width=tahvel.winfo_width()+tahvel.winfo_width()*faktor, height=tahvel.winfo_height()+tahvel.winfo_height()*faktor)
+#Lõikamine
+def lõika_pilt(event):
+    global hiir_x, hiir_y, ehiir_x, ehiir_y, algus_x, algus_y
+    hiire_positsioon(event)
+    if algus_x < 0 and algus_y < 0:
+        algus_x=hiir_x
+        algus_y=hiir_y
+    if hiir_x!=ehiir_x or hiir_y!=ehiir_y:
+        tahvel.delete("ajutine")
+    tahvel.create_rectangle(algus_x,algus_y,hiir_x,hiir_y,fill="",outline="gray",tag="ajutine")
+def lõika_pilt_reset(event):
+    global algus_x, algus_y, hiir_x, hiir_y
+    if algus_x>hiir_x and algus_y>hiir_y:
+        joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],"#FFFFFF")
+    elif algus_x>hiir_x and algus_y<hiir_y:
+        joonista.rectangle([(hiir_x,algus_y),(algus_x,hiir_y)],"#FFFFFF")
+    elif algus_x<hiir_x and algus_y>hiir_y:
+        joonista.rectangle([(algus_x,hiir_y),(hiir_x,algus_y)],"#FFFFFF")
+    else:
+        joonista.rectangle([(algus_x,algus_y),(hiir_x,hiir_y)],"#FFFFFF")
+    algus_x=-1
+    algus_y=-1
+    kuva_pilt(pilt)
+#Pööramine
+def pildi_pööramine():
+    global pilt
+    pilt=pilt.rotate(90, expand=1)
+    tahvel.delete("all")
+    tahvel.config(width=tahvel.winfo_x()+pilt.size[0], height=tahvel.winfo_y()+pilt.size[1])
+    tahvel.update()
+    kuva_pilt(pilt)
 #Pliiatsiga joonistamine
 def pliiats(event):
-    global paksus, värv, hiir_x, hiir_y, delay
-    delay=0.001
+    global paksus, värv, hiir_x, hiir_y, ehiir_x, ehiir_y
     hiire_positsioon(event)
-    tahvel.create_line(ehiir_x,ehiir_y,hiir_x,hiir_y, width=paksus.get(), fill=värv.get())
+    joonista.line([ehiir_x, ehiir_y, hiir_x, hiir_y], värv.get(), paksus.get())
+    kuva_pilt(pilt)
 #Pintsliga joonistamine
-def pintsel(event):
-    global paksus, värv, hiir_x, hiir_y, delay
-    delay=0.001
+def kujund(event):
+    global paksus, värv, hiir_x, hiir_y, ehiir_x, ehiir_y, algus_x, algus_y
     hiire_positsioon(event)
-    tahvel.create_oval(hiir_x,hiir_y,hiir_x+paksus.get(),hiir_y+paksus.get(),fill=värv.get(),outline=värv.get())
+    if algus_x < 0 and algus_y < 0:
+        algus_x=hiir_x
+        algus_y=hiir_y
+    if hiir_x!=ehiir_x or hiir_y!=ehiir_y:
+        tahvel.delete("ajutine")
+    if kujundi_piir.get() == "Piirjoon":    tahvel.create_rectangle(algus_x,algus_y,hiir_x,hiir_y,fill="",outline=värv.get(),tag="ajutine")
+    if kujundi_piir.get() == "Kuju":    tahvel.create_rectangle(algus_x,algus_y,hiir_x,hiir_y,fill=värv.get(),tag="ajutine")
+def kujund_reset(event):
+    global algus_x, algus_y, hiir_x, hiir_y
+    if algus_x>hiir_x and algus_y>hiir_y:
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],None,värv.get())
+        if kujundi_piir.get() == "Kuju":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],värv.get())
+        if kujundi_piir.get() == "Piiriga kuju":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],värv.get(),värv.get())
+    elif algus_x>hiir_x and algus_y<hiir_y:
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(hiir_x,algus_y),(algus_x,hiir_y)],None,värv.get())
+        if kujundi_piir.get() == "Kuju":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],värv.get())
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],None,värv.get())
+    elif algus_x<hiir_x and algus_y>hiir_y:
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(algus_x,hiir_y),(hiir_x,algus_y)],None,värv.get())
+        if kujundi_piir.get() == "Kuju":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],värv.get())
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],None,värv.get())
+    else:
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(algus_x,algus_y),(hiir_x,hiir_y)],None,värv.get())
+        if kujundi_piir.get() == "Kuju":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],värv.get())
+        if kujundi_piir.get() == "Piirjoon":    joonista.rectangle([(hiir_x,hiir_y),(algus_x,algus_y)],None,värv.get())
+    algus_x=-1
+    algus_y=-1
+    kuva_pilt(pilt)
 #Kirjutamine
 def tekst(event):
     global paksus, värv, hiir_x, hiir_y, sisend
-    tahvel.create_text(hiir_x,hiir_y,fill=värv.get(),text=sisend.get(),anchor=SW)
+    joonista.text((hiir_x,hiir_y), sisendtekst.get(), värv.get(), font=None, anchor=None)
+    kuva_pilt(pilt)
 #Pildi avamine
 def ava_pilt():
-    global pilt
+    global pilt, joonista
     dir=filedialog.askopenfilenames(filetypes=[("Image files", "*.gif;*.pgm;*.ppm;*.PNG;*.jpg"),("All files", "*.*")])
     if dir is None: return
     dir=str(dir)[2:len(str(dir))-3]
@@ -114,22 +242,24 @@ def ava_pilt():
         img_dir=img_dir[img_dir.find("/")+1:len(img_dir)]
     if not Path(img_dir).is_file(): shutil.copy(dir, sys.path[0])
     pilt = Image.open(img_dir)
-    img = ImageTk.PhotoImage(pilt)
+    tahvel.config(width=tahvel.winfo_x()+pilt.size[0], height=tahvel.winfo_y()+pilt.size[1])
+    joonista = ImageDraw.Draw(pilt)
+    tahvel.delete("all")
+    tahvel.update()
+    kuva_pilt(pilt)
+#Teeb PIL image TK photoimage-ks ja kuvab selle tahvlile
+def kuva_pilt(p):
+    img = ImageTk.PhotoImage(p)
     a=Label(image=img)
     a.image = img
-    tahvel.create_image(100, 100, image=img, anchor=NW)
+    tahvel.create_image(tahvel.winfo_width()/2-img.width()/2, tahvel.winfo_height()/2-img.height()/2, image=img, anchor=NW)
 #Pildi salvestamine
 def salvesta_pilt():
-    x=raam.winfo_x()+tahvel.winfo_x()
-    y=raam.winfo_y()+tahvel.winfo_y()
-    x1=x+tahvel.winfo_width()
-    y1=y+tahvel.winfo_height()
-    dir=filedialog.asksaveasfile(defaultextension=".gif", filetypes=[("GIF image", "*.gif"), ("PNG image","*.png"), ("JPG image", "*.jpg")])
+    dir=filedialog.asksaveasfile(defaultextension=".gif", filetypes=[("GIF image", "*.gif"), ("PNG image","*.png")])
     dir=dir.name
     print(dir)
     if dir is None: return
-    ImageGrab.grab().crop((x,y,x1,y1)).save(dir)
-    #img.save(dir)
+    pilt.save(dir)
     
 init_GUI()
 raam.mainloop()
